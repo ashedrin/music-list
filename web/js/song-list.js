b6e32e8a008edd7246726b92a36fdf75f6f60ce3
Alex Shedrin <alex.shedrin.ne@gmail.com>
2016-07-02 23:14:04 +0600

    Написал сортировку в таблице

--- web/js/song-list.js
+++ web/js/song-list.js
@@ -1,11 +1,43 @@
-var SongList = React.createClass({
+let SORT_TRIGGER_PERFORMER = 'performer';
+let SORT_TRIGGER_SONG = 'title';
+let SORT_TRIGGER_GENRE = 'genre';
+let SORT_TRIGGER_YEAR = 'year';
+
+let SORT_TRIGGER_DIRECTION_ASC = 'asc';
+let SORT_TRIGGER_DIRECTION_DESC = 'desc';
+
+
+let SongList = React.createClass({
 
     getInitialState: function() {
-        return { songs: [], loading: false };
+        return { 
+            songs: [], 
+            loading: false, 
+            sort: { 
+                trigger: SORT_TRIGGER_PERFORMER, 
+                direction: SORT_TRIGGER_DIRECTION_ASC
+            }
+        };
     },
 
     componentDidMount: function() {
         this.loadSongs();
+    },
+
+    componentWillUpdate: function(nextProps, nextState) {
+        this.sort(nextState.sort);
+    },
+
+    sort: function(sort) {
+        let $this = this;
+        this.state.songs.sort(function(prevSong, nextSong){
+            if(prevSong[sort.trigger] == nextSong[sort.trigger]) { return 0; }
+            if(sort.direction == SORT_TRIGGER_DIRECTION_ASC) {
+                return prevSong[sort.trigger] > nextSong[sort.trigger] ? 1 : -1;
+            } else {
+                return prevSong[sort.trigger] > nextSong[sort.trigger] ? -1 : 1;
+            }
+        });
     },
 
     componentDidUpdate: function(prevProps, prevState) {
@@ -23,35 +55,96 @@ var SongList = React.createClass({
             $this.setState({ songs: response, loading: false });
         });
     },
+    
+    clickSortTrigger: function(event) {
+        let trigger = event.currentTarget.getAttribute('data-sort-trigger');
+        this.setState(
+            {
+                sort: {
+                    trigger,
+                    direction: this.state.sort.direction == 'asc' ? 'desc': 'asc'
+                }
+            }
+        );
+    },
+
+    getSortTriggersCSSClasses: function() {
+        let performerSortTriggerCSSClass = "col-sm-3 col";
+        performerSortTriggerCSSClass +=
+            this.state.sort.trigger == SORT_TRIGGER_PERFORMER ? ' sort-' + this.state.sort.direction : '';
+
+        let songSortTriggerCSSClass = "col-sm-5 col";
+        songSortTriggerCSSClass +=
+            this.state.sort.trigger == SORT_TRIGGER_SONG ? ' sort-' + this.state.sort.direction : '';
+
+        let genreSortTriggerCSSClass = "col-sm-2 col";
+        genreSortTriggerCSSClass +=
+            this.state.sort.trigger == SORT_TRIGGER_GENRE ? ' sort-' + this.state.sort.direction : '';
+
+        let yearSortTriggerCSSClass = "col-sm-2 col";
+        yearSortTriggerCSSClass +=
+            this.state.sort.trigger == SORT_TRIGGER_YEAR ? ' sort-' + this.state.sort.direction : '';
+
+        return {
+            performerSortTriggerCSSClass,
+            songSortTriggerCSSClass,
+            genreSortTriggerCSSClass,
+            yearSortTriggerCSSClass
+        };
+    },
 
     render: function() {
+
+        let {
+            performerSortTriggerCSSClass,
+            songSortTriggerCSSClass,
+            genreSortTriggerCSSClass,
+            yearSortTriggerCSSClass
+        } = this.getSortTriggersCSSClasses();
+
         return (
             <div className="col-sm-9 pull-right">
                 <h3>Песни</h3>
                 <div className="music-list">
                     <div className="row headline">
-                        <div className="col-sm-4 col">
+                        <div
+                            className={performerSortTriggerCSSClass}
+                            data-sort-trigger={SORT_TRIGGER_PERFORMER}
+                            onClick={this.clickSortTrigger}
+                        >
                             Исполнитель
                             <div className="sort-group">
                                 <i className="fa fa-caret-up" />
                                 <i className="fa fa-caret-down" />
                             </div>
                         </div>
-                        <div className="col-sm-4 col">
+                        <div
+                            className={songSortTriggerCSSClass}
+                            data-sort-trigger={SORT_TRIGGER_SONG}
+                            onClick={this.clickSortTrigger}
+                        >
                             Песня
                             <div className="sort-group">
                                 <i className="fa fa-caret-up" />
                                 <i className="fa fa-caret-down" />
                             </div>
                         </div>
-                        <div className="col-sm-2 col">
+                        <div
+                            className={genreSortTriggerCSSClass}
+                            data-sort-trigger={SORT_TRIGGER_GENRE}
+                            onClick={this.clickSortTrigger}
+                        >
                             Жанр
                             <div className="sort-group">
                                 <i className="fa fa-caret-up" />
                                 <i className="fa fa-caret-down" />
                             </div>
                         </div>
-                        <div className="col-sm-2 col">
+                        <div
+                            className={yearSortTriggerCSSClass}
+                            data-sort-trigger={SORT_TRIGGER_YEAR}
+                            onClick={this.clickSortTrigger}
+                        >
                             Год
                             <div className="sort-group">
                                 <i className="fa fa-caret-up" />
--- web/js/song.js
+++ web/js/song.js
@@ -2,8 +2,8 @@ var Song = React.createClass({
     render: function() {
         return (
             <div className="row item">
-                <div className="col-sm-4">{this.props.song.performer}</div>
-                <div className="col-sm-4">{this.props.song.title}</div>
+                <div className="col-sm-3">{this.props.song.performer}</div>
+                <div className="col-sm-5">{this.props.song.title}</div>
                 <div className="col-sm-2">{this.props.song.genre}</div>
                 <div className="col-sm-2">{this.props.song.year}</div>
             </div>
